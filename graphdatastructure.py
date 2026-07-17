8bd988f4fbcdb557c0ee48520d82187bf035f859
zarifkhan-07 <zarifsuhana1@gmail.com>
2026-07-17 21:49:55 +0600

    Add files via upload

---
+++ graphdatastructure.py
@@ -0,0 +1,26 @@
+graph={}
+
+def add_node(node):
+    if node not in graph:
+        graph[node]=[]
+    
+def add_edges(node1,node2):
+    add_node(node1)
+    add_node(node2)
+
+    graph[node1].append(node2)
+    graph[node2].append(node1)
+
+def display_graph():
+    print("Adjacency list:\n")
+
+    for node in graph:
+        print(node,"=>",graph[node])
+
+add_edges("A","B")
+add_edges("B","C")
+add_edges("C","A")
+add_edges("B","D")
+add_edges("C","D")
+
+display_graph()
